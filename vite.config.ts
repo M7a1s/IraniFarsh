4d6a9271a90070f4fa8b6845830a88f00bd19624
M7a1s <yoarta87@gmail.com>
2025-08-08 23:30:24 +0330

    change loc .env

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  envDir: './src',
+  envDir: './',
   base: '/IraniFarsh/',
   resolve: {
     alias: {
